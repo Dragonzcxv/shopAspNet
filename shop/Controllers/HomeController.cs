801b450dcbb78bcbb2ae80923d3b6f6f79c8100c
Сергей Хохлов <Dragonzcxv@gmail.com>
2020-06-14 21:06:05 +0300

    AddHomePage

---
+++ shop/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shop.Interfaces;
+using shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+
+            var homeCars = new HomeViewModel
+            {
+                FavCars = _carRep.GetFavCars
+            };
+
+            return View(homeCars);
+
+        }
+
+    }
+}
---
+++ shop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop.ViewModels
+{
+    public class HomeViewModel
+    {
+
+        public IEnumerable<Car> FavCars { get; set; }
+
+    }
+}
